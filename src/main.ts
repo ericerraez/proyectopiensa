83f2257de4108e74f7835da311239ea2e97ba05f
ericerraez <103052520+ericerraez@users.noreply.github.com>
2023-02-13 22:00:02 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,10 @@ async function bootstrap() {
   .setDescription ('The median API description')
   .setVersion('0.1')
   .build();
-
   const document = SwaggerModule.createDocument(app,config);
   SwaggerModule.setup('api', app,document);
-<<<<<<< HEAD
   await app.listen(process.env.PORT || 3000);}
 bootstrap();
 =======
 await app.listen(process.env.PORT || 3000);}
 bootstrap();
->>>>>>> 94fda7c2765df0de1f89a96793e9b9453a67859e
